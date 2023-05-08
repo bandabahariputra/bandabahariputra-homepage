7485f60afd678120a1d2d3635eb9b77af364610b
Banda Bahari Putra <bandabahariputra99@gmail.com>
2023-05-08 13:13:13 +0700

    Add scroll to top when route change

--- src/router/index.js
+++ src/router/index.js
@@ -16,6 +16,9 @@ const router = createRouter({
       component: ProjectView,
     },
   ],
+  scrollBehavior(to, from, savedPosition) {
+    return { top: 0, behavior: 'smooth' };
+  },
 });
 
 export default router;
