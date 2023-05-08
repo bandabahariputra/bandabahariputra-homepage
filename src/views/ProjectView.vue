0e6561f59bff59555ad0ac1959c7480db9d28996
Banda Bahari Putra <bandabahariputra99@gmail.com>
2023-05-08 21:37:06 +0700

    Change icons

--- src/views/ProjectView.vue
+++ src/views/ProjectView.vue
@@ -44,19 +44,7 @@ const project = getProject(route.params.slug);
             target="_blank"
             class="flex items-center gap-2 text-orange-400 underline-offset-4 hover:underline dark:text-purple-600"
             >{{ project.demo.replace('https://', '') }}
-            <svg viewBox="0 0 24 24" focusable="false" class="h-4 w-4">
-              <g
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-width="2"
-              >
-                <path
-                  d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"
-                ></path>
-                <path d="M15 3h6v6"></path>
-                <path d="M10 14L21 3"></path>
-              </g></svg
+            <i class="ph-bold ph-arrow-square-out"></i
           ></a>
         </div>
       </div>
